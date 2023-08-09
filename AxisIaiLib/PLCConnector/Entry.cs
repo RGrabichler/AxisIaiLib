c4ed6728296c6e40653caba16ba5b4821e2df716
Roman <roman.grabichler@gema-technik.de>
2023-08-09 08:13:39 +0200

    init

---
+++ AxisIaiLib/PLCConnector/Entry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Vortex.Adapters.Connector.Tc3.Adapter;
+
+namespace PLCConnector
+{
+    public class Entry
+    {
+        public static readonly string AmsId = "192.168.4.1.1.1";
+
+        public static PLC.PLCTwinController Plc { get; } =
+            new PLC.PLCTwinController(Tc3ConnectorAdapter.Create(AmsId, 851, true));
+    }
+}
