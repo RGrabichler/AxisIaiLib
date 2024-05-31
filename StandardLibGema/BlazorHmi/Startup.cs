85b4f2c6b958800f72698ea6e5b2add7730a9b96
roman <romangrabicherl@gmail.com>
2024-05-31 10:25:44 +0200

    init

---
+++ StandardLibGema/BlazorHmi/Startup.cs
@@ -0,0 +1,57 @@
+using BlazorHmi.Data;
+using Vortex.Presentation.Blazor.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StandardLibGema;
+
+namespace BlazorHmi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
+            services.AddSingleton<WeatherForecastService>();
+            services.AddVortexBlazorServices();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+            Entry.Plc.Connector.BuildAndStart();
+        }
+    }
+}
---
+++ StandardLibGema/StandardLibGemaConnector/Entry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using StandardLibGema;
+
+using Vortex.Adapters.Connector.Tc3.Adapter;
+
+namespace StandardLibGema
+{
+    public class Entry
+    {
+        public static readonly string AmsId = "39.235.213.61.1.1";
+
+        public static StandardLibGemaTwinController Plc { get; } =
+            new StandardLibGemaTwinController(Tc3ConnectorAdapter.Create(AmsId, 852, true));
+    }
+}
